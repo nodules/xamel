b5eb2a511a8a57d0136ab3c611abd4d613f8146c
Phillip Kovalev <kaero@yandex-team.ru>
2013-05-13 17:00:07 +0400

    fix #1 add simple CData support

---
+++ test/cdata.js
@@ -0,0 +1,26 @@
+var CData = require('../lib/xml').CData,
+    test = {};
+
+test['CData (constructor)'] = function(beforeExit, assert) {
+    var cdata = new CData('Hello!');
+
+    assert.strictEqual(typeof cdata, 'object');
+    assert.strictEqual(cdata instanceof CData, true);
+};
+
+test['CData (data)'] = function(beforeExit, assert) {
+    var DATA = 'Hello!',
+        cdata = new CData(DATA);
+
+    assert.strictEqual(cdata.data, DATA);
+};
+
+test['CData (toString)'] = function(beforeExit, assert) {
+    var DATA = 'Hello!',
+        cdata = new CData(DATA);
+
+    assert.strictEqual(String(cdata), DATA);
+    assert.strictEqual('' + cdata, DATA);
+};
+
+module.exports = test;
