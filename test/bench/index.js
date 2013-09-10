e998abe4f178e382ecfadf61eb645c2faa0f2e77
Phillip Kovalev <kaero@yandex-team.ru>
2013-09-10 04:09:49 +0400

    expat support mostly done

--- test/bench/index.js
+++ test/bench/index.js
@@ -1,14 +1,16 @@
 var xamel = require('../../lib/xamel'),
-    xmlString = require('fs').readFileSync(__dirname + '/../data/simple.xml', 'utf8'),
+    xmlString = require('fs').readFileSync(__dirname + '/../data/simple.xml'),
     count,
     executions = 0,
     TOTAL_EXECUTIONS = 10,
     total = [],
     last,
-    ts;
+    ts,
+    parsers = [ 'sax', 'expat'],
+    parser = 0;
 
 function parse(done) {
-    xamel.parse(xmlString, function(error) {
+    xamel.parse(xmlString.toString(), { parser : parsers[parser] }, function(error) {
         if (error) {
             throw error;
         }
@@ -17,7 +19,7 @@ function parse(done) {
         last = process.hrtime(ts);
 
         if (last[0] < 1) {
-            setImmediate(parse.bind(this, done));
+            setImmediate(function() { parse(done); });
         } else {
             done(count * (1 - last[1] * 1e-9));
         }
@@ -32,7 +34,7 @@ function testRun(done) {
         total.push(count);
 
         if (executions++ < TOTAL_EXECUTIONS) {
-            setImmediate(testRun.bind(this, done));
+            setImmediate(function() { testRun(done); });
         } else {
             done(Math.floor(total.reduce(function(pv, cv) {
                 return pv + cv;
@@ -41,6 +43,15 @@ function testRun(done) {
     });
 }
 
-testRun(function(result) {
-    console.log('%d ops/sec, %d test runs', result, TOTAL_EXECUTIONS);
-});
+function testDone(result) {
+    console.log('%s: %d ops/sec, %d test runs', parsers[parser], result, TOTAL_EXECUTIONS);
+
+    if (++parser < parsers.length) {
+        executions = 0;
+        testRun(testDone);
+    } else {
+        console.log('done.');
+    }
+}
+
+testRun(testDone);
--- test/parse.js
+++ test/parse.js
@@ -2,70 +2,73 @@ var test = require('./lib/testFile').bind(module),
     assert = require('chai').assert,
     xamel = require('../lib/xamel');
 
-test(
-    'xamel.parse (simple.xml)',
-    ['simple.xml', 'simple.json'],
-    function(files, done) {
-        var xml = files[0],
-            json = files[1];
+[ 'sax', 'expat'].forEach(function(parser) {
 
-        xamel.parse(xml, function(error, result) {
-            //console.log(JSON.stringify(result));
-            assert.deepEqual(JSON.parse(json), JSON.parse(JSON.stringify(result)), 'xml & json assertion');
+    test(
+        'xamel.parse (simple.xml) @' + parser,
+        ['simple.xml', 'simple.json'],
+        function(files, done) {
+            var xml = files[0],
+                json = files[1];
 
-            done();
-        });
-    });
-
-test(
-    'xamel.parse (partial tree building)',
-    ['simple.xml', 'partial.json', 'partial2.json'],
-    function(files, done) {
-        var xml = files.shift(),
-            jsons = files,
-            buildPaths = [
-                'menu/food/customer',
-                'menu/food/customer/*/text()'
-            ],
-            assertions = 0;
-
-        jsons.forEach(function(json, idx) {
-            xamel.parse(xml, { buildPath : buildPaths[idx] }, function(error, result) {
+            xamel.parse(xml, { parser : parser }, function(error, result) {
                 //console.log(JSON.stringify(result));
                 assert.deepEqual(JSON.parse(json), JSON.parse(JSON.stringify(result)), 'xml & json assertion');
 
-                if (++assertions === jsons.length) {
-                    done();
-                }
+                done();
             });
         });
-    });
 
-test(
-    'xamel.parse (cdata parsing)',
-    ['simple.xml', 'cdata.json'],
-    function(files, done) {
-        var xml = files[0],
-            json = files[1];
+    test(
+        'xamel.parse (partial tree building) @' + parser,
+        ['simple.xml', 'partial.json', 'partial2.json'],
+        function(files, done) {
+            var xml = files.shift(),
+                jsons = files,
+                buildPaths = [
+                    'menu/food/customer',
+                    'menu/food/customer/*/text()'
+                ],
+                assertions = 0;
 
-        xamel.parse(xml, { buildPath : '*/*/*/cdata()', cdata : true }, function(error, result) {
-            //console.log(JSON.stringify(result));
-            assert.deepEqual(JSON.parse(json), JSON.parse(JSON.stringify(result)), 'xml & json assertion');
+            jsons.forEach(function(json, idx) {
+                xamel.parse(xml, { parser : parser, buildPath : buildPaths[idx] }, function(error, result) {
+                    //console.log(JSON.stringify(result));
+                    assert.deepEqual(JSON.parse(json), JSON.parse(JSON.stringify(result)), 'xml & json assertion');
 
-            done();
+                    if (++assertions === jsons.length) {
+                        done();
+                    }
+                });
+            });
         });
-    });
 
-test(
-    'xamel.parse (broken xml)',
-    ['broken.xml'],
-    function(files, done) {
-        var xml = files[0];
+    test(
+        'xamel.parse (cdata parsing) @' + parser,
+        ['simple.xml', 'cdata.json'],
+        function(files, done) {
+            var xml = files[0],
+                json = files[1];
 
-        xamel.parse(xml, function(error, result) {
-            assert.ok(error instanceof Error, 'error passed');
-            assert.strictEqual(result, null, 'result is null');
+            xamel.parse(xml, { parser : parser, buildPath : '*/*/*/cdata()', cdata : true }, function(error, result) {
+                //console.log(JSON.stringify(result));
+                assert.deepEqual(JSON.parse(json), JSON.parse(JSON.stringify(result)), 'xml & json assertion');
 
-            done();
+                done();
+            });
         });
-    });
+
+    test(
+        'xamel.parse (broken xml) @' + parser,
+        ['broken.xml'],
+        function(files, done) {
+            var xml = files[0];
+
+            xamel.parse(xml, { parser : parser }, function(error, result) {
+                assert.ok(error instanceof Error, 'error passed');
+                assert.strictEqual(result, null, 'result is null');
+
+                done();
+            });
+        });
+});
